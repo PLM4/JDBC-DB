f36cd7c224d724d5275a74479448b76c75bb230e
PLM4 <pedrolucasmelo051@gmail.com>
2025-04-28 16:54:47 -0300

    Adicionando controller a main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import controller.JDBCController;
+
 public class Main {
 
     public static void main(String[] args) {
-    
+        JDBCController control = new JDBCController(null, null, null, null);
+        control.start();
     }
 }
